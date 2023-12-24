fa4950ff2821ac8b760257424b7ec0b53b1546fd
Pedro Henrique <phlferezin03@gmail.com>
2023-12-24 17:28:06 -0300

    feat: adicionando label a HomeViewController e separando responsabilidades de adicionar subviews e constraints.

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text =  "Hello, World!"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor(named: "BackgroundColor")
+        
+        addSubviews()
+        setUpConstraints()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(label)
+    }
+    
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
 
 }
