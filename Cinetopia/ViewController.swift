836799d4bfaddbf59d539d2b47750313b0240d52
Pedro Henrique <phlferezin03@gmail.com>
2023-12-24 16:29:29 -0300

    feat: adicionados assets de cores e imagens no projeto.

---
+++ Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Pedro Henrique on 24/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
