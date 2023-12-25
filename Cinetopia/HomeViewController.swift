ce040d64732b561747fab4e0297276af109d5c98
Pedro Henrique <phlferezin03@gmail.com>
2023-12-24 21:29:25 -0300

    feat: criando os componentes da home screen e os adicionando na tela.

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -9,33 +9,77 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-    private lazy var label: UILabel = {
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "Logo"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var coupleImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "Couple"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var welcomeLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text =  "Hello, World!"
+        label.text = "O lugar ideal para buscar, salvar e organizar seus filmes favoritos!"
         label.textColor = .white
-        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20, weight: .bold)
         return label
     }()
-
+    
+    private lazy var welcomeButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Quero começar!", for: .normal)
+        button.backgroundColor = UIColor(named: "ButtonBackgroundColor")
+        button.setTitleColor(UIColor(named: "BackgroundColor"), for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.layer.cornerRadius = 32
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "BackgroundColor")
         
-        addSubviews()
+        addSubviews([
+            logoImageView,
+            coupleImageView,
+            welcomeLabel,
+            welcomeButton,
+        ])
         setUpConstraints()
     }
     
-    private func addSubviews() {
-        view.addSubview(label)
+    private func addSubviews(_ subViews: [UIView]) {
+        subViews.forEach { view in
+            self.view.addSubview(view)
+        }
     }
     
     private func setUpConstraints() {
         NSLayoutConstraint.activate([
-            label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 64),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            coupleImageView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 32),
+            coupleImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            welcomeLabel.topAnchor.constraint(equalTo: coupleImageView.bottomAnchor, constant: 32),
+            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            welcomeButton.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 32),
+            welcomeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 64),
+            welcomeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -64),
+            welcomeButton.heightAnchor.constraint(equalToConstant: 64),
         ])
     }
-
+    
 }
 
